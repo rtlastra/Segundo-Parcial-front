79f5d9bc489afd7f411c137a4e2b855cb885b9ad
rtlastra <rtlastra@uninorte.edu.co>
2020-04-20 00:02:57 -0500

    Employees

--- Parcial1-front/public/app.js
+++ Parcial1-front/public/app.js
@@ -79,23 +79,36 @@ function login() {
 
         }, function (reason) {
             var db = firebase.firestore();
+            var sw=false;
             db.collection("Operator").where("email", "==", email2).where("password", "==", password2)
                 .get()
                 .then(function (querySnapshot) {
+                    
                     querySnapshot.forEach(function (doc) {
+                        sw=true;
                         // doc.data() is never undefined for query doc snapshots
                         console.log(doc.data().name);
 
                         if (doc.data().name) {
-                            window.location.href = ('Cuestionario.html');
+                            window.location.href = ('Cuestionario.html?operator='+doc.id);
+                           
+                           
                         } else {
                             alert("Fallo en la autenticacion");
                         }
                     });
+                    if(!sw){
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Usuario no encontrado',
+                        });
+                    }
                 })
                 .catch(function (error) {
                     console.log("Error getting documents: ", error);
                 });
+                
+        
         });
 }
 function obs() {
@@ -200,7 +213,122 @@ function preguntas() {
 
 }
 
+// funcion donde creo el cuestionario por empresa
+function cuestionario(uid) {
+    var db = firebase.firestore();
+    let p1 = document.getElementById('question1');
+    let p2 = document.getElementById('question2');
+    let p3 = document.getElementById('question3');
+    let p4 = document.getElementById('question4');
+    let p5 = document.getElementById('question5');
+    let p11 = document.getElementById('p11');
+    let p12 = document.getElementById('p12');
+    let p13 = document.getElementById('p13');
+    let p21 = document.getElementById('p21');
+    let p22 = document.getElementById('p22');
+    let p23 = document.getElementById('p23');
+    let p31 = document.getElementById('p31');
+    let p32 = document.getElementById('p32');
+    let p33 = document.getElementById('p33');
+    let p41 = document.getElementById('p41');
+    let p42 = document.getElementById('p42');
+    let p43 = document.getElementById('p43');
+    let p51 = document.getElementById('p51');
+    let p52 = document.getElementById('p52');
+    let p53 = document.getElementById('p53');
 
+
+    db.collection("Questions").where("Company", "==", uid)
+        .onSnapshot((querySnapshot) => {
+            p1.innerHTML = '';
+            p2.innerHTML = '';
+            p3.innerHTML = '';
+            p4.innerHTML = '';
+            p5.innerHTML = '';
+            p12.innerHTML = '';
+            p13.innerHTML = '';
+            p11.innerHTML = '';
+            p21.innerHTML = '';
+            p22.innerHTML = '';
+            p23.innerHTML = '';
+            p31.innerHTML = '';
+            p32.innerHTML = '';
+            p33.innerHTML = '';
+            p41.innerHTML = '';
+            p42.innerHTML = '';
+            p43.innerHTML = '';
+            p51.innerHTML = '';
+            p52.innerHTML = '';
+            p53.innerHTML = '';
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                // console.log(doc.data().name);
+                var str = doc.data().Question;
+                var res = str.split("|");
+                var str1 = doc.data().Answer1;
+                var res1 = str1.split("|");
+                var str2 = doc.data().Answer2;
+                var res2 = str2.split("|");
+                var str3 = doc.data().Answer3;
+                var res3 = str3.split("|");
+                p1.innerHTML += `<h4>${res[0]}</h4>`
+                p2.innerHTML += `<h4>${res[1]}</h4>`
+                p3.innerHTML += `<h4>${res[2]}</h4>`
+                p4.innerHTML += `<h4>${res[3]}</h4>`
+                p5.innerHTML += `<h4>${res[4]}</h4>`
+                p11.innerHTML = res1[0];
+                p12.innerHTML = res2[0];
+                p13.innerHTML = res3[0];
+                p21.innerHTML = res1[1];
+                p22.innerHTML = res2[1];
+                p23.innerHTML += res3[1];
+                p31.innerHTML += res1[2];
+                p32.innerHTML += res2[2];
+                p33.innerHTML += res3[2];
+                p41.innerHTML += res1[3];
+                p42.innerHTML += res2[3];
+                p43.innerHTML += res3[3];
+                p51.innerHTML += res1[4];
+                p52.innerHTML += res2[4];
+                p53.innerHTML += res3[4];
+            });
+        })
+}
+
+function rcuppass() {
+    var correo = document.getElementById("edit4").value;//aqui iria el campo de donde se lee el correo
+    firebase.auth().sendPasswordResetEmail(correo)
+        .then(function () {
+            // Password reset email sent.
+        })
+        .catch(function (error) {
+            // Error occurred. Inspect error.code.
+        });
+}
+
+function config(){
+    let s=window.location.search.split('=');
+    if(!s[1]){
+        location.replace("index.html");
+    }else{
+        var db = firebase.firestore();
+        db.collection("Operator").doc(s[1])
+            .get()
+            .then(function (doc) {
+                //console.log(doc.data().uid);
+                cuestionario(doc.data().uid);
+                let lista = document.getElementById('prob');
+                lista.innerHTML=doc.data().name;
+            })
+            .catch(function (error) {
+                alert('Error al eliminar usuario');
+            })   
+    }
+}
+
+function close(){
+    window.location.href('index.html');
+}
 
 
 
